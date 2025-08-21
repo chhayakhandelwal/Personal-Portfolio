f3e294647f56e2a716bb5af186a84c2c20a80e21
chhavi khandelwal <102668248+chhayakhandelwal@users.noreply.github.com>
2025-08-21 22:28:15 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll when clicking navbar links
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
